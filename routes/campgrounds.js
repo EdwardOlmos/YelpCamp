e179b096ac368f1916ad9f5254f0136178bad895
Edward Olmos <edwardolmos3@gmail.com>
2021-06-24 23:32:08 -0700

    refactored routes for readability

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,21 +2,29 @@ const express = require("express");
 const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
-const Campground = require("../models/campground");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
-router.get("/", catchAsync(campgrounds.index));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-);
-
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, catchAsync(campgrounds.deleteCampground));
 
 router.get(
   "/:id/:edit",
@@ -24,15 +32,5 @@ router.get(
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
 );
-
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
-
-router.delete("/:id", isLoggedIn, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const User = require("../models/user");
 const users = require("../controllers/users");
 const catchAsync = require("../utils/catchAsync");
 
-router.get("/register", users.renderRegisterForm);
+router
+  .route("/register")
+  .get(users.renderRegisterForm)
+  .post(catchAsync(users.register));
 
-router.post("/register", catchAsync(users.register));
-
-router.get("/login", users.renderLoginForm);
-
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-  }),
-  users.login
-);
+router
+  .route("/login")
+  .get(users.renderLoginForm)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.login
+  );
 
 router.get("/logout", users.logout);
 
